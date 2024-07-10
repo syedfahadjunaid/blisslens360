f30177b8afa0f6c0d8058e799aceb3c6b6a62ae7
Syed Fahad Junaid <syedfahadjunaid@gmail.com>
2024-07-10 13:58:47 +0530

    commit

--- src/components/home/HeroSection/HeroSection.jsx
+++ src/components/home/HeroSection/HeroSection.jsx
@@ -1,16 +1,102 @@
 import React from "react";
+import { MdArrowOutward } from "react-icons/md";
 import heroBackgroundImage from "../../../assets/heroBackground.png";
+import designImage5k from "../../../assets/5kDesignImage.png";
+import heroCarouselFrame from "../../../assets/heroCarouselFrame.png";
+import image1 from "../../../assets/carousel/image1.png";
+
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 const HeroSection = () => {
   return (
     <div
-      className="w-full h-screen flex items-center"
+      className="w-full flex flex-col items-center"
       style={{
         background: `url('${heroBackgroundImage}')`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "100%",
+        backgroundPositionY: "8rem",
       }}
-    ></div>
+    >
+      <div className="flex justify-end w-full mt-[5rem] pr-[2rem]">
+        <div className="flex flex-col headingFont text-start">
+          <p className="text-[#FFBB00] pl-[10px]">5k+</p>
+          <p className="text-[#6D6D6D] pl-[10px]">weddings</p>
+          <img src={designImage5k} alt="designImage" className="w-[100px]" />
+        </div>
+      </div>
+      <div className="w-[90%] flex flex-row items-start">
+        <div className="w-[50%] flex flex-col gap-[1rem]">
+          <h1 className="headingFont text-[80px] text-start leading-[5rem]">
+            Crafting Everlasting Moments
+          </h1>
+          <p className="text-start text-[#4F4F4F]">
+            The Joy of Connection: How Making Moments Matters strengthens
+            Relationships
+          </p>
+          <button className="flex justify-center w-fit text-white border bg-[#FFBB01] p-[1rem] rounded-[30px] flex items-center gap-[10px]">
+            <p>Book Now</p>
+            <MdArrowOutward />
+          </button>
+          <div className="flex justify-center w-full mt-[5rem]">
+            <div className="flex flex-col headingFont text-start">
+              <p className="text-[#FFBB00] pl-[10px]">3k+</p>
+              <p className="text-[#6D6D6D] pl-[10px]">Events</p>
+              <img
+                src={designImage5k}
+                alt="designImage"
+                className="w-[150px]"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="w-[50%] h-fit bg-gray flex justify-center items-center relative">
+          <img
+            src={heroCarouselFrame}
+            className="w-[450px] h-[450px]"
+            alt="heroCarouselFrameImage"
+          />
+          <CarouselProvider
+            naturalSlideWidth={100}
+            naturalSlideHeight={80}
+            totalSlides={3}
+            className="w-[70%] absolute"
+          >
+            <Slider>
+              <Slide index={0}>
+                <img src={image1} alt="image1" />
+              </Slide>
+              <Slide index={1}>
+                <img src={image1} alt="image2" />
+              </Slide>
+              <Slide index={2}>
+                <img src={image1} alt="image3" />
+              </Slide>
+            </Slider>
+            <div className="w-full flex justify-center">
+              <div className="border border-solid border-[#FFBB01] w-fit flex items-center justify-center rounded-full h-[40px] w-[40px]">
+                <ButtonBack>
+                  <IoIosArrowBack />
+                </ButtonBack>
+                <ButtonNext>
+                  <IoIosArrowForward />
+                </ButtonNext>
+              </div>
+            </div>
+          </CarouselProvider>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,85 +14,89 @@ const Navbar = () => {
 
   const activeStyle = "text-yellow absolute text-center w-full h-fit";
   return (
-    <div className="flex items-center w-[90%] justify-between shadow-md relative">
-      <div className="flex w-full justify-end pr-[5rem]">
-        <img
-          src={navBackgroundImage}
-          alt="navigation-background-image"
-          //   className="pr-[10rem]"
-        />
-      </div>
-      <div className="flex items-center justify-between w-full absolute">
-        <div className="flex items-center gap-[2rem]  p-[1rem]">
+    <div className="fixed top-0 w-full flex items-center justify-center z-10">
+      <div className="flex items-center w-[90%] justify-between shadow-md relative bg-white">
+        <div className="flex w-full justify-end pr-[5rem]">
           <img
-            src={logo}
-            alt="blisslens_logo"
-            className=""
-            onClick={() => navigate("/")}
+            src={navBackgroundImage}
+            alt="navigation-background-image"
+            //   className="pr-[10rem]"
           />
-          <div className="border-solid border-l-[2px] pl-[2rem] flex gap-[2rem] items-center">
-            {location.pathname === "/" ? (
-              <div className="relative w-fit flex items-center justify-center h-full">
-                <img
-                  src={linkBorderImage}
-                  alt="link_border"
-                  className="w-[65px] h-[65px]"
-                />
+        </div>
+        <div className="flex items-center justify-between w-full absolute p-[1rem]">
+          <div className="flex items-center gap-[2rem] py-[1rem]">
+            <img
+              src={logo}
+              alt="blisslens_logo"
+              className=""
+              onClick={() => navigate("/")}
+            />
+            <div className="border-solid border-l-[2px] pl-[2rem] flex gap-[2rem] items-center">
+              {location.pathname === "/" ? (
+                <div className="relative w-fit flex items-center justify-center h-full">
+                  <img
+                    src={linkBorderImage}
+                    alt="link_border"
+                    className="w-[65px] h-[65px]"
+                  />
+                  <Link
+                    to={"/"}
+                    className={
+                      location.pathname === "/" ? activeStyle : "text-gray"
+                    }
+                  >
+                    Home
+                  </Link>
+                </div>
+              ) : (
                 <Link
                   to={"/"}
                   className={
-                    location.pathname === "/" ? activeStyle : "text-gray"
+                    location.pathname === "/"
+                      ? activeStyle
+                      : "text-gray w-[65px]"
                   }
                 >
                   Home
                 </Link>
-              </div>
-            ) : (
-              <Link
-                to={"/"}
-                className={
-                  location.pathname === "/" ? activeStyle : "text-gray w-[65px]"
-                }
-              >
-                Home
-              </Link>
-            )}
-            {location.pathname === "/portfolio" ? (
-              <div className="relative w-fit flex items-center justify-center h-full">
-                <img
-                  src={linkBorderImage}
-                  alt="link_border"
-                  className="w-[65px] h-[65px]"
-                />
+              )}
+              {location.pathname === "/portfolio" ? (
+                <div className="relative w-fit flex items-center justify-center h-full">
+                  <img
+                    src={linkBorderImage}
+                    alt="link_border"
+                    className="w-[65px] h-[65px]"
+                  />
+                  <Link
+                    to={"/"}
+                    className={
+                      location.pathname === "/portfolio"
+                        ? activeStyle
+                        : "text-gray"
+                    }
+                  >
+                    Portfolio
+                  </Link>
+                </div>
+              ) : (
                 <Link
-                  to={"/"}
+                  to={"/portfolio"}
                   className={
                     location.pathname === "/portfolio"
                       ? activeStyle
-                      : "text-gray"
+                      : "text-gray w-[65px]"
                   }
                 >
                   Portfolio
                 </Link>
-              </div>
-            ) : (
-              <Link
-                to={"/portfolio"}
-                className={
-                  location.pathname === "/portfolio"
-                    ? activeStyle
-                    : "text-gray w-[65px]"
-                }
-              >
-                Portfolio
-              </Link>
-            )}
+              )}
+            </div>
           </div>
+          <button className="flex justify-center border border-black p-[1rem] rounded-[30px] flex items-center gap-[10px]">
+            <p>Book Now</p>
+            <MdArrowOutward />
+          </button>
         </div>
-        <button className="flex justify-center border border-black p-[1rem] rounded-[30px] flex items-center gap-[10px]">
-          <p>Book Now</p>
-          <MdArrowOutward />
-        </button>
       </div>
     </div>
   );
